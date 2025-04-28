93a811335ebbdc980652679f197250cce1ea22fb
Lujain <115418326+Im-Lujain@users.noreply.github.com>
2025-04-28 17:16:16 +0300

    update_app.sh

--- update_app.sh
+++ update_app.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
-
-set -e  # Exit on any error
+set -e
 
 date
 echo "Updating Python application on VM..."
-
-REPO_URL="github.com/stantaov/chatbot-project.git"
-BRANCH="main"
-GITHUB_TOKEN=$TOKEN  # Passed securely via protectedSettings
+USER=$(whoami)
 HOME_DIR=$(eval echo ~$USER)
 APP_DIR="$HOME_DIR/chatbot-project"
+REPO_URL="https://github.com/Im-Lujain/chatbot-project.git"
+BRANCH="stage-6.5-tf"
+GITHUB_TOKEN=$TOKEN  # Passed securely via protectedSettings
 
 # Update code
 if [ -d "$APP_DIR" ]; then
-    sudo -u azureuser bash -c "cd $APP_DIR && git fetch origin && git reset --hard origin/$BRANCH"
+    sudo -u $USER bash -c "cd $APP_DIR && git fetch origin && git reset --hard origin/$BRANCH"
 else
-    sudo -u azureuser git clone -b "$BRANCH" "https://${GITHUB_TOKEN}@${REPO_URL}" "$APP_DIR"
+    sudo -u $USER  git clone -b "$BRANCH" "https://${GITHUB_TOKEN}@${REPO_URL}" "$APP_DIR"
 fi
 
 # Install dependencies
-sudo -u azureuser $HOME_DIR/miniconda3/envs/project/bin/pip install --upgrade pip
-sudo -u azureuser $HOME_DIR/miniconda3/envs/project/bin/pip install -r "${APP_DIR}/requirements.txt"
+sudo -u $USER  $HOME_DIR/miniconda3/envs/project/bin/pip install --upgrade pip
+sudo -u $USER  $HOME_DIR/miniconda3/envs/project/bin/pip install -r "${APP_DIR}/requirements.txt"
 
-# Restart services and check status
+# Restart the service
 sudo systemctl restart backend
 sudo systemctl is-active --quiet backend || echo "Backend failed to start"
 sudo systemctl restart frontend
-sudo systemctl is-active --quiet frontend || echo "Frontend failed to start"
+sudo systemctl is-active --quiet frontend || echo "frontend failed to start"
 
 echo "Python application update completed!"
